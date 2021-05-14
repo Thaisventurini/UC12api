e8a725bb3183f62b1d4a94eda5509e6eacd150eb
Thais Venturini <venturinibrunohais@gmail.com>
2021-05-14 14:20:31 -0300

    Arrumando

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express =require ("express");
 const mongoose = require ("mongoose")
+const routes = require("./routes");
 const app = express ();
+app.use(express.json());
+app.use(routes);
 mongoose.conect("mongedb://localhost:27017/apiUC12", {useNewUrlParser: true, useUnifiedTopoly: true});
-app.get("/",(req,res)=> {
-    res.send ("Olá, mundo");
-})
 app.listen (3001);
