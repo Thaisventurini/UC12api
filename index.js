b7ac4837cd42369b907678650e5cb0b9687cc566
Thais Venturini <venturinibrunohais@gmail.com>
2021-05-14 14:07:35 -0300

    Introdução

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =require ("express");
+const mongoose = require ("mongoose")
 const app = express ();
+mongoose.conect("mongedb://localhost:27017/apiUC12", {useNewUrlParser: true, useUnifiedTopoly: true});
 app.get("/",(req,res)=> {
     res.send ("Olá, mundo");
 })
